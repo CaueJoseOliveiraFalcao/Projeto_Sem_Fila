54c24496979f3d8525093d690ae4fd195a832b0f
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2024-03-15 13:04:43 -0400

    store-resgiter-completed

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,18 +1,50 @@
+import { query } from "express"
 import { db } from "../connectdb.js"
-
+import  bcrypt  from 'bcrypt'
 export const  register = async (req,res) =>{
-    const {userName , email , password} = req.body
-
-    if (!userName){
-        return res.status(422).json({msg: 'nome obrigatorio'})
-    }
-    if (!email){
-        return res.status(422).json({msg: 'email obrigatorio'})
-    }
-    if (!password){
-        return res.status(422).json({msg : 'senha obrigatorio'})
-    }
     
+    if (!req.body){
+        return res.status(422).json({msg: 'Entrada Vazia'})
+    }
+    else{
+        const {name , cnpj , password , imgProfile , store_desc} = req.body
+        if (!name){
+            return res.status(422).json({msg: 'nome obrigatorio'})
+        }
+        if (!cnpj){
+            return res.status(422).json({msg: 'cnpj obrigatorio'})
+        }
+        if (!password){
+            return res.status(422).json({msg : 'senha obrigatorio'})
+        }
+        
+        db.query('SELECT cnpj FROM stores WHERE cnpj = ?' , [cnpj] , async(error , data) => {
+            if (error) { 
+                console.log(error)
+                return res.status(500).json({msg : error});
+            }
+            if (data.length > 0){
+                console.log('cnpj existente');
+                return res.status(500).json({msg : 'cnpj existente'});
+            }
+            else{
+                const passwordHashed = await bcrypt.hash(password , 10);
+                db.query('INSERT INTO stores SET ?' , {name , cnpj , password:passwordHashed , imgProfile , store_desc} , (error) => {
+                    if (error){
+                        console.log(error)
+                        return res.status(500).json({msg : error});
+                    }
+                    else{
+                        console.log('cadastro efetuado')
+                        return res.status(200).json({msg : 'cadastro efetudado'});
+                    }
+                })
+            }
+
+        })
+        
+    }
+
 
 } 
 export const  login = (req,res) =>{
--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routes/auth.js'
-
+import bodyParser from 'body-parser';
 const app = express();
 
 app.use(cors())
+app.use(bodyParser.urlencoded({extended : false}));
 app.use('/api/auth' , authRouter);
 
 app.listen(8082 , () =>{
