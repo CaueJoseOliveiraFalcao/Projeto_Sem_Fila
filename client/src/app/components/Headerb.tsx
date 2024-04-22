d9a8e32d441c0f434639c984e5853f56ced4d10e
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2024-04-22 16:27:17 -0400

    icon front

--- client/src/app/components/Headerb.tsx
+++ client/src/app/components/Headerb.tsx
@@ -7,7 +7,10 @@ export default function Headerb() {
         <nav  className="bg-red-600 p-5 mt-0 w-full">
             <div className="container mx-auto flex flex-wrap items-center">
                 <div className="flex w-full md:w-1/2 justify-between items-center">
-                    <a href='/' className="text-white text-xl font-bold">Queue Fila UE?</a>
+                <div className='flex items-center'>
+                        <img width={75} height={75} src="../mascote_icone.png" />
+                        <a href='/'className="text-white text-xl font-bold">Queue Fila UE?</a>
+                    </div>
                 <button
                     className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none"
                     type="button"
--- client/src/app/components/Headerp.tsx
+++ client/src/app/components/Headerp.tsx
@@ -11,7 +11,11 @@ export default function Headerp() {
         <nav  className="bg-red-600 p-5 mt-0 w-full">
             <div className="container mx-auto flex flex-wrap items-center">
                 <div className="flex w-full md:w-1/2 justify-between items-center">
-                    <a href='/'className="text-white text-xl font-bold">Queue Fila UE?</a>
+                    <div className='flex items-center'>
+                        <img width={75} height={75} src="../mascote_icone.png" alt="" srcset="" />
+                        <a href='/'className="text-white text-xl font-bold">Queue Fila UE?</a>
+                    </div>
+
                 <button
                     className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none"
                     type="button"
